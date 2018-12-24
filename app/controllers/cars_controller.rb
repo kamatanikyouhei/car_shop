16eafde176165bc3695c5cd99b7cea7328f160ca
kamatanikyouhei <kamatanikyouhei@kamatanikyouheinoMacBook-ea.local>
2018-12-25 05:13:11 +0900

    登録

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,20 @@ class CarsController < ApplicationController
 def index
   @cars = Car.all
 end
+def new
+  @car = Car.new
+end
+
+def create
+  @car = Car.new(car_params)
+  @car.save
+  redirect_to @car, notice: "新しい車を登録しました。"
+end
+
+private
+
+def car_params
+  params.reqire(:car).permit(:name, :price, :release_date, :description)
+end
+
 end
